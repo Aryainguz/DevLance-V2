7856d90e57b1f081f1f6b16b0cb8c26b396d0af3
Aryainguz <aryaninguz369@gmail.com>
2024-01-03 07:35:00 +0530

    added flex-slider style

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.use(express.static(__dirname +"/Public"));
+app.use(express.static(__dirname +"/public"));
 app.set('views', __dirname+'/views/');
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
