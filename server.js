749f0a0843f2d339c54716803ff8c0934c3031a3
Aryainguz <aryaninguz369@gmail.com>
2024-01-04 03:47:56 +0530

    changed static directory

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ require("dotenv").config()
 const API_KEY = process.env.SENDGRID_API_KEY
 sgMail.setApiKey(API_KEY)
 const app = express()
-app.use(express.static(path.join(__dirname +"/Public")));
+app.use(express.static(path.join(__dirname +"Public")));
 app.set('views', __dirname+'/views/');
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
