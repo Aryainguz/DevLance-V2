80db584231c6e84569f5d7c212adf80f57bb6b91
Aryainguz <aryaninguz369@gmail.com>
2024-01-03 06:14:09 +0530

    added middlewares

--- server.js
+++ server.js
@@ -2,14 +2,16 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 require("dotenv").config()
+const app = express();
+
+const port = process.env.PORT || 3000;
+
 app.use(express.static(__dirname +"/Public"));
 app.set('views', __dirname+'/views/');
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser())
 
-
-const app = express();
-app.listen(process.env.PORT || 3000, () => {
+app.listen(port, () => {
     console.log(`Server ready at ${port}`)
 })
