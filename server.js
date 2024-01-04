859d5bc5ff2758be972aa48170b36bb086decc5a
Aryan Inguz <117257768+Aryainguz@users.noreply.github.com>
2024-01-05 00:18:17 +0530

    removed unusable modules

--- server.js
+++ server.js
@@ -7,8 +7,6 @@ const sgMail = require("@sendgrid/mail")
 const crypto = require('crypto')
 const cookieParser = require("cookie-parser")
 const mongoose = require("mongoose");
-const { response } = require("express");
-const path = require("path");
 
 require("dotenv").config()
 
